cdc3d226a4623f841e3aa35d7155e9d784a2801a
Atanasius Raditya Herkristito <atanasiusditya@gmail.com>
2024-08-12 12:50:17 +0700

    feat: week 4 module

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { StyleSheet, View, Text } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
+import userData from "./data.json";
+import styles from "./App.styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Atanasius Raditya Herkristito - 0000044898</Text>
-    </View>
+    <ScrollView>
+      {userData.map((users) => {
+        return (
+          <View style={styles.container} key={users.name}>
+            <View style={styles.card}>
+              <Image
+                source={{
+                  uri: users.photo_url,
+                }}
+                style={styles.avatar}
+              />
+              <View style={styles.boldText}>
+                <Text style={styles.boldText}>{users.name}</Text>
+                <Text>{users.email}</Text>
+              </View>
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-  },
-});
