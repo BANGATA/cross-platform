1dc15166c764d311cd2d3fa19a2dc01ed3f3c3c2
Atanasius Raditya Herkristito <atanasiusditya@gmail.com>
2024-08-16 17:37:27 +0700

    feat: camera

--- App.js
+++ App.js
@@ -1,9 +1,94 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Platform,
+  PermissionsAndroid,
+  Image,
+} from "react-native";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
 
 export default function App() {
+  const [uri, setUri] = useState("");
+  const openImagePicker = () => {
+    launchImageLibrary(
+      {
+        mediaType: "photo",
+        includeBase64: false,
+        maxHeight: 2000,
+        maxWidth: 2000,
+      },
+      handleResponse
+    );
+  };
+
+  const handleCameraLaunch = () => {
+    launchCamera(
+      {
+        mediaType: "photo",
+        includeBase64: false,
+        maxHeight: 2000,
+        maxWidth: 2000,
+      },
+      handleResponse
+    );
+  };
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: "Camera Permission",
+          message: "This app needs access to your camera to take photos.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK",
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Camera permission granted");
+        handleCameraLaunch();
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const handleResponse = (response) => {
+    if (response.didCancel) {
+      console.log("User cancelled image picker");
+    } else if (response.error) {
+      console.log("Image picker error: ", response.error);
+    } else if (response.assets && response.assets.length > 0) {
+      const imageUri = response.assets[0].uri;
+      setUri(imageUri);
+    } else {
+      console.log("No assets found in the response");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Atanasius Raditya Herkristito - 0000044898</Text>
+      <Button
+        title="Open Camera"
+        onPress={() => {
+          if (Platform.OS === "android") {
+            requestCameraPermission();
+          } else {
+            handleCameraLaunch();
+          }
+        }}
+      />
+      <Button title="Open Galery" onPress={openImagePicker} />
+      {uri && (
+        <Image source={{ uri: uri }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 }
