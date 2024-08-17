43c78a487575f95ddefcd5b2838b2e15eddc7c77
Atanasius Raditya Herkristito <atanasiusditya@gmail.com>
2024-08-17 13:50:21 +0700

    feat: firebase

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import { StyleSheet, View, Text } from "react-native";
+import { addDoc, collection } from "firebase/firestore";
+import { StyleSheet, View, Text, Button, Platform } from "react-native";
+import { db } from "./utils/firebaseConfig";
 
 export default function App() {
+  const addData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Vincentius",
+        last: "Kurniawan",
+        born: "2024",
+      });
+      if (Platform.OS === "android") {
+        console.log("Document written from phone with ID: ", docRef.id);
+      } else {
+        console.log("Document written with ID: ", docRef.id);
+      }
+    } catch (err) {
+      console.error("Error occured ", err);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text>Atanasius Raditya Herkristito - 0000044898</Text>
+      <Button title="Add User" onPress={addData} />
     </View>
   );
 }
